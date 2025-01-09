dd1f4e5698bf053f259a61420855e8384e9120e9
Nimtharu Jayasekara <98390833+gnjayasekara@users.noreply.github.com>
2025-01-09 07:46:00 +0530

    Delete client directory

--- client/pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-      <div>
-        <h1>Welcome to Next.js!</h1>
-        <p>Start building your app with Next.js.</p>
-      </div>
-    );
-  }
-  
