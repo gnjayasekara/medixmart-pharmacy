6bc9c5dc18ecc01267e63d39aaf0848d851d5c12
Nimtharu jayasekara <sagn.jayasekara@gmail.com>
2025-01-28 10:30:24 +0530

    Feat: Homepage featured products

---
+++ frontend/components/featured.tsx
@@ -0,0 +1,94 @@
+"use client"
+
+import * as React from "react"
+import Image from "next/image"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import useEmblaCarousel from "embla-carousel-react"
+import Autoplay from "embla-carousel-autoplay"
+
+export function FeaturedItems() {
+  const autoplay = Autoplay({ delay: 3000, stopOnInteraction: true });
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [autoplay]);
+
+
+  return (
+    <Carousel 
+    plugins={[autoplay]}
+    ref={emblaRef} className="w-[70vw] mx-auto"
+    onMouseEnter={autoplay.stop}
+    onMouseLeave={autoplay.reset}
+    >
+      <CarouselContent>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/4">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-auto items-center justify-center p-6">
+                  <Image  
+                    src={`/products/Mega-Check-Blood-Pressure-Monitoring-System_Medixmart.jpg?height=300&width=300&text=Slide ${1}`}
+                    alt={`Slide ${1}`}
+                    width={200}
+                    height={200}
+                    className="rounded-md object-cover"
+                  />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/4">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-auto items-center justify-center p-6">
+                  <Image  
+                    src={`/pharmacy-2.jpg?height=300&width=300&text=Slide ${2}`}
+                    alt={`Slide ${2}`}
+                    width={300}
+                    height={300}
+                    className="rounded-md object-cover"
+                  />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/4">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-auto items-center justify-center p-6">
+                  <Image  
+                    src={`/medixmart-background-2.jpg?height=300&width=300&text=Slide ${1}`}
+                    alt={`Slide ${1}`}
+                    width={1000}
+                    height={1000}
+                    className="rounded-md object-cover"
+                  />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/4">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-auto items-center justify-center p-6">
+                  <Image  
+                    src={`/medixmart-background-3.jpg?height=300&width=300&text=Slide ${1}`}
+                    alt={`Slide ${1}`}
+                    width={1000}
+                    height={1000}
+                    className="rounded-md object-cover"
+                  />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
