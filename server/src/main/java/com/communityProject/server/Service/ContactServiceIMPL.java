4053fad5876328b351b7766fb33685f6d1c6c80e
ChethanaWijesinghe10 <chethanawijesinghe10@gmail.com>
2025-02-08 17:21:35 +0530

    Updated Contact form

---
+++ server/src/main/java/com/communityProject/server/Service/ContactServiceIMPL.java
@@ -0,0 +1,25 @@
+package com.communityProject.server.Service;
+
+import com.communityProject.server.Entity.Contact;
+import com.communityProject.server.Repo.ContactRepository;
+import org.hibernate.annotations.SecondaryRow;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactServiceIMPL implements ContactService{
+    @Autowired
+    private ContactRepository contactRepository;
+
+    // Implement the method to save contact data
+    @Override
+    public Contact saveContact(Contact contact) {
+        return contactRepository.save(contact);
+    }
+
+    // Implement the method to retrieve all contacts (optional)
+   /* @Override
+    public List<Contact> getAllContacts() {
+        return contactRepository.findAll();
+    }*/
+}
