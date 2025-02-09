b7463d8d75b1d22815bdb60a1158713ee35e41f2
ChethanaWijesinghe10 <chethanawijesinghe10@gmail.com>
2025-02-09 20:23:55 +0530

    Resolved conflicts and merged latest changes

--- server/src/main/java/com/communityProject/server/Service/PrescriptionService.java
+++ server/src/main/java/com/communityProject/server/Service/PrescriptionService.java
@@ -1,8 +1,6 @@
 package com.communityProject.server.Service;
 
 
-import com.communityProject.server.DTO.PrescriptionDTO;
-import com.communityProject.server.Entity.Prescription;
 
 public interface PrescriptionService {
    String savePrescription(PrescriptionDTO prescriptionDTO);
